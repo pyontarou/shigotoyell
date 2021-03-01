7587b98610f9d9b6d5f72f6479fccc4c1d789de1
pyontarou <kooooo7150@gmail.com>
2021-03-01 11:33:21 +0900

    バリデーションの記述

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,24 @@ class Job < ApplicationRecord
   belongs_to :companies
   belongs_to :user
 
+  with_options presence: true do
+    validates :job_description
+    validates :qualification
+    validates :workplace
+    validates :salary
+    validates :bonus
+    validates :working_hours
+  end  
+
+  with_options numericality: { other_than: 1 } do
+    validates :recruitment_type_id
+    validates :employment_status_id
+    validates :holiday_id
+    validates :experience＿id
+    validates :educational_background_id
+    validates :age_requirements_id
+  end
+
+  
+
 end
