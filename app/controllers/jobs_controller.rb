2c8d12b64409dcfa31ffb68b3a01c8eb492529f6
pyontarou <kooooo7150@gmail.com>
2021-03-01 21:53:15 +0900

    createアクションの設定

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :authenticate_company!, except: [:show, :index]
 
   def index
     @jobs = Job.all
@@ -11,6 +12,17 @@ class JobsController < ApplicationController
   end
 
   def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to company_path(@current_company)
+    else
+     render :new
+    end
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:recruitment_type_id, :job_description, :qualification, :workplace, :salary, :bonus, :employment_status_id, :working_hours, :holiday_id, :experience＿id, :educational_background_id, :age_requirements_id).merge(company_id: current_company.id)
     
   end
 end
