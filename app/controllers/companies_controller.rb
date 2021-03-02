1c9f44a3ca5602176da56d6b9888812602d9d33f
pyontarou <kooooo7150@gmail.com>
2021-03-02 10:53:56 +0900

    userコントローラーの記述修正

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :authenticate_company!
+
   def show
     @companies = Company.all
     @company = Company.new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
   def show
-    @company = Company.find(params[:id])
+
+    
   end
 end
